b50dabcbac75cd0ae32a040fe1e1578bf88e8a6a
unknown <matheusdamasceno15@hotmail.com>
2022-02-22 16:12:50 -0300

    Consumindo service e criando a funcao de deletar a categoria

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
 
 @Component({
   selector: 'app-category-list',
@@ -7,14 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  public categoreis: Category[] = [];
+
+  constructor(private categoryservice: CategoryService) { }
 
   ngOnInit(): void {
+    this.categoryservice.getAll().subscribe(
+      categoreis => this.categoreis = categoreis,
+      error => alert('Error a o carregar a lista')
+    )
   }
 
-  public alert(): any {
 
-    window.alert('excluir')
+  public deletarCategorias(category): any {
+
+    const mustDelete = confirm('Deseja realmente excluir este item?')
+
+    if (mustDelete) {
+      this.categoryservice.delete(category.id).subscribe(
+        () => this.categoreis = this.categoreis.filter(element => element != category),
+        () => alert('Error a o excluir')
+      )
+    }
   }
 
 }

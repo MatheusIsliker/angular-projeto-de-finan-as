93ec71c020d0277d98ee32f09d9435f0d7fcdef8
unknown <matheusdamasceno15@hotmail.com>
2022-04-11 17:30:05 -0300

    relatorio de despesas

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Category } from '../../categories/shared/category.model';
+import { CategoryService } from '../../categories/shared/category.service';
+import { Entry } from '../../entries/shared/entry.module';
+import { EntryService } from '../../entries/shared/entry.service';
+import   currencyFormatter from 'currency-formatter'
 
 @Component({
   selector: 'app-reports',
@@ -7,24 +12,98 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
+  public expenseTotal: any = 0;
+  public revenueTotal: any = 0;
+  public balance: any = 0;
+  public expenseChartData: any;
+  public revenueChartData: any;
+  public chartOptions = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+  public categories: Category[] = [];
+  public entries: Entry[] = [];
+  @ViewChild('month') month: ElementRef = null;
+  @ViewChild('year') year: ElementRef = null;
 
-  public mesesEanos: Array<any> = [
-    {mes:'Janeiro' , ano: 'Ano de 2012' , id: 1},
-    {mes:'Fevereiro' , ano: 'Ano de 2013' , id: 2},
-    {mes:'Março' , ano: 'Ano de 2014' , id: 3},
-    {mes:'Abril' , ano: 'Ano de 2015' , id: 4},
-    {mes:'Maio' , ano: 'Ano de 2016' , id: 5},
-    {mes:'Junho' , ano: 'Ano de 2017' , id: 6},
-    {mes:'Julho' , ano: 'Ano de 2018' , id: 7},
-    {mes:'Agosto' , ano: 'Ano de 2019' , id: 8},
-    {mes:'Setembro' , ano: 'Ano de 2020' , id: 9},
-    {mes:'Novembro' , ano: 'Ano de 2021' , id: 10},
-    {mes:'Dezembro' , ano: 'Ano de 2022' , id: 11}
-  ]
 
-  constructor() { }
+  constructor(private categoryService: CategoryService, private entryService: EntryService ) { }
 
   ngOnInit(): void {
+    this.categoryService.getAll().subscribe(categories => this.categories = categories)
   }
 
+  public generateReports(): any {
+         const month = this.month.nativeElement.value;
+         const year = this.year.nativeElement.value;
+
+         if(!month || !year) {
+             alert('Você precisa selecionar o mês e o ano para gerar os relatórios')
+         } else {
+           this.entryService.getByMontAndYear(month, year).subscribe(this.setValues.bind(this))
+         }
+  }
+
+  private setValues(entries: Entry[]): any  {
+        this.entries = entries;
+        this.calculateBalance();
+        this.setChartData();
+  }
+
+  private calculateBalance(): any {
+         let expensiveTotal = 0;
+         let revenueTotal = 0;
+         this.entries.forEach(entry => {
+           if(entry.type == 'revenue') {
+                revenueTotal += currencyFormatter.unformat(entry.amount, { code: 'BRL'}) 
+           } else {
+                expensiveTotal += currencyFormatter.unformat(entry.amount, { code: 'BRL'}) 
+           }
+         });
+
+         this.expenseTotal  = currencyFormatter.format(expensiveTotal, { code: 'BRL'});
+         this.revenueTotal  = currencyFormatter.format(revenueTotal, { code: 'BRL'});
+         this.balance = currencyFormatter.format(revenueTotal - expensiveTotal, { code: 'BRL'})
+  }
+
+    private setChartData(): any {
+
+      this.revenueChartData = this.getChartData('revenue', 'Grafico de Receitas',  '#9CCC65'),
+      this.expenseChartData = this.getChartData('revenue', 'Grafico de Depesas',  '#e03131')
+    }
+
+    private getChartData(entryType: string, title: string, color: string): any {
+      const chartData = [];
+      this.categories.forEach(categories => {
+        // filtering entries by category and type
+        const filteringEntries  = this.entries.filter(
+          entry => (entry.categoryId  == categories.id) && (entry.type == entryType) 
+        );
+
+        // if found entries, then sum entries amount and add to chartData
+        if(filteringEntries.length > 0) {
+             const totalAmount = filteringEntries.reduce(
+               (total, entry) => total + currencyFormatter.unformat(entry.amount, { code: 'BRL'}), 0
+             )
+
+             chartData.push({
+               categoryName: categories.name,
+               totalAmount: totalAmount
+             })
+        }
+      });
+           return {
+             labels: chartData.map(item => item.categoryName),
+             datasets: [{
+               label: title, 
+               backgroundColor: color,
+               data: chartData.map(item => item.totalAmount)
+             }]
+           }
+    }
 }

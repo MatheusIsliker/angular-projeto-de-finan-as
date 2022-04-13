f812d0b8eba90ad36f1da64207b17180217bef34
unknown <matheusdamasceno15@hotmail.com>
2022-04-13 16:33:57 -0300

    finalizando o projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ const routes: Routes = [
   { path: 'entries', loadChildren: () => import('src/app/pages/entries/entries.module').then(m => m.EntriesModule) },
   { path: 'categories', loadChildren: () => import('src/app/pages/categories/categories.module').then(m => m.CategoriesModule) },
   { path: 'reports', loadChildren: () => import('src/app/pages/reports/reports.module').then(m => m.ReportsModule) },
+
+  {path: '', redirectTo: '/reports', pathMatch: 'full'}
 ];
 
 @NgModule({
